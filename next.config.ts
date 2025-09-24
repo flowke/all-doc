57358d83d96ded43000e27b40420ce57f6f0efaa
zhaihongjie <zhaihongjie@100tal.com>
2025-09-24 14:02:59 +0800

    upd

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+import nextra from 'nextra'
+ 
+// Set up Nextra with its configuration
+const withNextra = nextra({
+  // ... Add Nextra-specific options here
+})
+ 
+// Export the final Next.js config with Nextra included
+export default withNextra({
+  // ... Add regular Next.js options here
+  turbopack:{
+    resolveAlias: {
+      'next-mdx-import-source-file': './mdx-components.tsx'
+    }
+  }
+})
